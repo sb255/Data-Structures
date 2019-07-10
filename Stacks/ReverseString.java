5e3122d29b371f56a7455ee75a29587267321f5a
sb255 <samarthbais1@gmail.com>
2019-07-10 12:28:11 +0530

    Stack

---
+++ Stacks/ReverseString.java
@@ -0,0 +1,31 @@
+//Reverse the order of words in a String using Stack!
+
+import java.util.*;
+
+
+public class Test{
+        
+    public static void main(String[] args){
+            
+        String intro = "How are you doing today";
+        
+        Stack<String> st = new Stack<String>(); 
+        
+        for(String s : intro.split(" ")){
+        
+            st.push(s); 
+            
+        }
+        
+        int size = st.size(); 
+        
+        for(int i=0; i<size; i++){
+        
+            System.out.print(st.pop()+" ");
+            
+        }
+    }
+        
+}
+
+
