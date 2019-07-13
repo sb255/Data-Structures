b89527ebb0d25ed39a2a8072cd409c5726589d01
sb255 <samarthbais1@gmail.com>
2019-07-13 15:30:37 +0530

    Depth First Search

---
+++ Trees/PreOrderTravelsal.java
@@ -0,0 +1,42 @@
+/* PreOrderTravelsal/DepthFirstSearch without Recursion */
+
+class Node{
+    Node left,right;
+    int dValue;
+    
+    Node(int v){
+        dValue = v;
+        left = null;
+        right = null;
+    }
+}
+
+
+public class Test {
+    public ArrayList<Integer> preorderTraversal(Node A) {
+        
+                
+        ArrayList<Integer> list = new ArrayList<>(); 
+        
+        Stack<Node> stack = new Stack<>(); 
+        
+        stack.push(A); 
+        
+        while(!stack.empty()){
+            
+            Node node = stack.peek(); 
+            list.add(node.dValue); 
+            stack.pop(); 
+            
+            if(node.right!=null)
+                stack.push(node.right); 
+                
+            if(node.left!=null)
+                stack.push(node.left); 
+        }
+        
+        return list; 
+        
+    }
+}
+
