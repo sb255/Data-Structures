cc793b48e08e15bc0ec7574e848a3567397c530a
sb255 <samarthbais1@gmail.com>
2019-07-14 10:33:02 +0530

    Print in reverse

---
+++ LinkedLists/PrintInReverse.java
@@ -0,0 +1,20 @@
+   void printInReverse(Node head) {
+        
+        Node n = head; 
+        Stack<Integer> stack = new Stack<>(); 
+
+        if(head.next==null)
+            System.out.println(n.data); 
+        
+        while(n.next!=null){
+            stack.push(n.data); 
+            n = n.next; 
+        }
+
+        stack.push(n.data); 
+
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+
+    }
