f86cd1fde8b7cde3e501ac84164fbbe5919f09fd
sb255 <samarthbais1@gmail.com>
2019-07-14 12:47:21 +0530

    Merge Two Sorted Lists

---
+++ LinkedLists/MergeTwoSortedLists.java
@@ -0,0 +1,51 @@
+public class Test {
+    public Node mergeTwoSortedLists(Node A, Node B) {
+        
+        LinkedList<Integer> list = new LinkedList<>(); 
+        
+        while(A.next!=null){
+            list.add(A.val); 
+            A = A.next; 
+        }
+        
+        list.add(A.val); 
+        
+        
+        while(B.next!=null){
+            list.add(B.val); 
+            B = B.next; 
+        }
+        
+        list.add(B.val); 
+        
+        Collections.sort(list); 
+        
+        
+        
+        Stack<Node> stack = new Stack<>(); 
+        Stack<Node> stackReverse = new Stack<>(); 
+        
+        
+        for(int i=0; i<list.size(); i++){
+            stack.push(new Node(list.get(i))); 
+        }
+        
+        for(int i=0; i<list.size(); i++){
+            stackReverse.push(stack.pop()); 
+        }
+        
+        
+        ListNode llist = stackReverse.pop(); 
+        
+        ListNode n = llist; 
+        
+        for(int i=1; i<list.size(); i++){
+            n.next = stackReverse.pop();
+            n = n.next; 
+        }
+        
+        
+        return llist; 
+        
+    }
+}
