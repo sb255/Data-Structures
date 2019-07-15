7b31d3ae5decb3fd7ea4d79295819ff18ff2537b
sb255 <samarthbais1@gmail.com>
2019-07-15 14:28:10 +0530

    Merging two sorted Lists using a LinkedList

--- LinkedLists/MergeTwoSortedLists.java
+++ LinkedLists/MergeTwoSortedLists.java
@@ -1,2 +1,36 @@
-public class Test{
+public class Test {
+    Node mergeTwoSortedLists(Node A, Node B) {
+        
+        LinkedList<Integer> list = new LinkedList<>(); 
+        
+        while(A.next!=null){
+            list.add(A.data); 
+            A = A.next; 
+        }
+        
+        list.add(A.data); 
+        
+        
+        while(B.next!=null){
+            list.add(B.data); 
+            B = B.next; 
+        }
+        
+        list.add(B.data); 
+        
+        Collections.sort(list); 
+        
+        Node llist = new Node(list.get(0)); 
+        
+        Node n = llist; 
+        
+        for(int i=1; i<list.size(); i++){
+            n.next = new Node(list.get(i));
+            n = n.next; 
+        }
+        
+        
+        return llist; 
+        
+    }
 }
