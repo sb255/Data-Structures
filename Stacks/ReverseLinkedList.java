4770d9e7864dfad801ffd7388c89706236ccd0a0
sb255 <samarthbais1@gmail.com>
2019-07-22 15:34:59 +0530

    Reversing a LinkedList using a Stack

--- Stacks/ReverseLinkedList.java
+++ Stacks/ReverseLinkedList.java
@@ -1,25 +1,87 @@
-/*-- Reversing a LinkedList or a Doubly LinkedList using Stack  --*/
+/*-- Reversing a LinkedList using Stack --*/
 
-    LinkedListNode reverseLinkedList(LinkedListNode head) {
+import java.util.*;
+
+public class Test {
+    
+    
+    
+ /*------------------- Reversing a LinkedList using Stack ------------------------------------------*/
+    
+    
+
+    static Node reverseLinkedList(Node head) {
 
         Stack<Integer> stack = new Stack<>();
 
-        LinkedListNode n = head; 
+        Node n = head; 
 
         while(n!=null){
             stack.push(n.data);
             n = n.next; 
         }
 
-        LinkedListNode newList = new LinkedListNode(stack.pop());
-        LinkedListNode nodesNewList = newList;  
+        Node newList = new Node(stack.pop());
+        Node nodesNewList = newList;  
 
         while(!stack.isEmpty()){
-            nodesNewList.next = new LinkedListNode(stack.pop()); 
+            nodesNewList.next = new Node(stack.pop()); 
             nodesNewList = nodesNewList.next; 
         }
 
         return newList;  
 
     }
+    
+ 
+    /*-------------------------------------------------------------------------------------------------*/
+ 
+    
+    /*-- Inputs from the main method --*/
+    
+    public static void main(String[] args){
+        
+        Node A = new Node(5);
+        A.addNodeToTail(10);
+        A.addNodeToTail(20);
+        A.addNodeToTail(30);
+        A.addNodeToTail(35);
+        A.addNodeToTail(40);
+        A.addNodeToTail(50);
+        
+        Node C = reverseLinkedList(A);
+        
+        while(C!=null){
+            System.out.println(C.data);
+            C = C.next; 
+        }
+    
+    }
+    
+}
+
+/*-- Node class Implementation --*/
+
+class Node{
+    
+    int data;
+    Node next; 
+    
+    public Node(int data){
+    this.data = data;
+    }
+    
+    public void addNodeToTail(int addData){
+        
+        Node current = this;
+        
+        while(current.next!=null){
+            current = current.next; 
+        }
+        
+        current.next = new Node(addData); 
+        
+    }
+    
+}
 
