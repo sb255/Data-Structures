e7d43fa640e0394c1f5129bf7289f39b1c4d58aa
sb255 <samarthbais1@gmail.com>
2019-07-22 16:14:06 +0530

    Removind duplicates from a LinkedList using HashSet

--- HashSets/DeleteDuplicates.java
+++ HashSets/DeleteDuplicates.java
@@ -1,33 +1,70 @@
-/*-- Application of a HashSet in a LinkedList for removing duplicate nodes  --*/
+import java.util.*;
 
-    LinkedListNode removeDuplicatesFromLinkedList(LinkedListNode head) {
+public class Test {
+    
+    /*-- Removing duplicates from Linked List method using HashSet --*/
+    static Node removeDuplicatesFromLinkedList(Node head) {
 
         if(head==null)
-            return null; 
-        
-        LinkedListNode n = head; 
-        LinkedHashSet<Integer> lhs = new LinkedHashSet<>(); 
+            return null;
+
+        Node n = head;
+        LinkedHashSet<Integer> lhs = new LinkedHashSet<>();
 
         while(n.next!=null){
-            lhs.add(n.data); 
-            n = n.next; 
+            lhs.add(n.data);
+            n = n.next;
         }
 
         lhs.add(n.data);
 
         Iterator<Integer> it = lhs.iterator();
 
-        LinkedListNode newHead = new LinkedListNode(it.next()); 
+        Node newHead = new Node(it.next());
 
-        LinkedListNode listNodes = newHead; 
-
+        Node listNodes = newHead;
+        
         while(it.hasNext()){
-            listNodes.next = new LinkedListNode(it.next()); 
-            listNodes = listNodes.next; 
+            listNodes.next = new Node(it.next());
+            listNodes = listNodes.next;
 
         }
 
-        return newHead; 
+        return newHead;
 
     }
+
+    
+    /*-- Inputs from the main method --*/
+    
+    public static void main(String[] args){
+        
+        Node A = new Node(10);
+        A.next = new Node(10);
+        A.next.next = new Node(20);
+        A.next.next.next = new Node(20);
+        
+        Node C = removeDuplicatesFromLinkedList(A);
+        
+        while(C!=null){
+            System.out.println(C.data);
+            C = C.next; 
+        }
+    
+    }
+    
+}
+
+/*-- Node class Implementation --*/
+
+class Node{
+    
+    int data;
+    Node next; 
+    
+    public Node(int data){
+    this.data = data;
+    }
+    
+}
 
