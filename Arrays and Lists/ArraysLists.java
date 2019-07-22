9d33929189dc400d8a3d082d34b9a5a0eb5bec4f
sb255 <samarthbais1@gmail.com>
2019-07-22 16:00:40 +0530

    Comparing two Lists

--- Arrays and Lists/ArraysLists.java
+++ Arrays and Lists/ArraysLists.java
@@ -1,21 +1,27 @@
-/*When we want to compare two list' objects, we use the following method: */
 
-static List<Integer> compareLists(List<Integer> a, List<Integer> b) {
+/*-- Comparing two lists' objects --*/
+
+import java.util.*;
+
+public class Test {
+
+    static List<Integer> compareLists(List<Integer> a, List<Integer> b) {
 
         List<Integer> list1 = new ArrayList<Integer>();
-        list1 = a;  
+        list1 = a;
 
         List<Integer> list2 = new ArrayList<Integer>();
-        list2 = b; 
+        list2 = b;
 
-        /* This line is tricky*/
- 
+        /* This line is tricky */
+
         /*
+        
         Integer[] arr1 = new Integer[list1.size];
-        arr1 = list1.toArray(); 
+        arr1 = list1.toArray();
         The above two lines can be merged and can be written as:
         Integer[] arr1 = list1.toArray(new Integer[list1.size()]);
-        
+
         */
 
         Integer[] arr1 = list1.toArray(new Integer[list1.size()]);
@@ -23,18 +29,18 @@ static List<Integer> compareLists(List<Integer> a, List<Integer> b) {
         Integer[] arr2 = list2.toArray(new Integer[list2.size()]);
 
         int aaa=0;
-        int bbb=0; 
-
+        int bbb=0;
+        
         for(int i=0; i<list1.size(); i++){
-            
-            if(arr1[i]==arr2[i]) 
+
+            if(arr1[i]==arr2[i])
             {}
 
             else if(arr1[i]>arr2[i])
             aaa++;
 
             else if(arr1[i]<arr2[i])
-            bbb++; 
+            bbb++;
         }
 
         List<Integer> arrFinal = new ArrayList<Integer>();
@@ -45,3 +51,33 @@ static List<Integer> compareLists(List<Integer> a, List<Integer> b) {
         return arrFinal;
 
 }
+    
+    /*-- Inputs from the main method --*/
+    
+    public static void main(String[] args){
+    
+        List<Integer> A = new ArrayList<>(new Integer(10));
+        A.add(20);
+        A.add(30);
+        A.add(40);
+        A.add(50);
+        
+        List<Integer> B = new ArrayList<>(new Integer(30));
+        B.add(40);
+        B.add(40);
+        B.add(40);
+        B.add(40);
+        
+        List<Integer> list = compareLists(A,B);
+        
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+        
+    }
+    
+}
+
+
+
+
