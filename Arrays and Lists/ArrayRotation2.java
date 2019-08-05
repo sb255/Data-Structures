f1c587feff6df03bf63e63c872504e26a0ff6f0a
sb255 <samarthbais1@gmail.com>
2019-08-05 11:45:28 +0530

    Array Rotation

--- Arrays and Lists/ArrayRotation2.java
+++ Arrays and Lists/ArrayRotation2.java
@@ -1,23 +1,24 @@
 /*
- * Array Rotaion using Loops.
- * Alternate solution using Queues: https://github.com/sb255/Data-Structures/blob/master/Queues/StringRotation.java 
+ * Array rotation using loops!
  */
 
 
-public class ArrayRotation {
+
+public class ArrayRotation2 {
     
     public static void main(String args[]){
         
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
         int[] arr2 = new int[10]; 
-        int rotation = 5;
+        int rotation = 21;
         
         //If roation is greater than the length of the array
         if(rotation>arr.length)
             rotation = rotation%arr.length; 
         
+            //Populate the new array with the value at the new index or the rotated index
             for(int i=0; i<arr.length; i++){
-                arr2[i] = arr[(i + arr.length - rotation) % arr.length]; 
+                arr2[(i+rotation)%arr.length] = arr[i]; 
             }
         
         for(int i=0; i<arr2.length; i++){
