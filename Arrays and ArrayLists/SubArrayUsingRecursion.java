1cb2b89db052e4136752f068c0a4e6aaff8f1bf7
sb255 <samarthbais1@gmail.com>
2019-11-08 12:38:05 +0530

    Sub array using recursion

---
+++ Arrays and ArrayLists/SubArrayUsingRecursion.java
@@ -0,0 +1,38 @@
+//-- CREATING SUB ARRAYS OF AN ARRAY USING RECURSION  
+
+public class Test {
+    
+    public static void subArrays(int[] arrr, int tip, int limit){
+        
+        if(tip>limit){
+        }
+        
+        else if(tip<limit){
+        
+            for(int i=tip; i<limit; i++){
+                System.out.print(arrr[i]+" ");
+            }
+            
+            System.out.println(" ");
+        
+            subArrays(arrr, tip+1, limit);
+            
+        }
+        
+        //-- WHEN THE TIP AND THE LIMIT ARE EQUAL
+        else{
+            subArrays(arrr, 0, limit - 1);
+        }
+        
+        
+    }
+    
+    public static void main(String[] args){
+        
+        //-- EXAMPLE ARRAY 
+        int[] arr = {1,2,3,4,5,6,7};
+        subArrays(arr, 0, arr.length);
+        
+    }
+}
+
